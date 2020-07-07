44bb644b53a4ebce95a541b23929475c0d600a5d
EzzU <ezzadeinol@gmail.com>
2020-07-07 10:45:49 +0530

    all of flutter files

---
+++ test1/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test1/states/app_state.dart';
+import 'screens/home.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  return runApp(MultiProvider(providers: [
+      ChangeNotifierProvider.value(value: AppState(),)
+  ],
+  child: MyApp(),));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'uber clone',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Uber clone'),
+    );
+  }
+}
